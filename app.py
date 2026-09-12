2616c626b3ed6936ddcf63125d3337e5b8886ab4
azharabbas414-cyber <azhar.abbas414@gmail.com>
2026-09-12 13:25:22 +0500

    Update app.py

--- app.py
+++ app.py
@@ -196,7 +196,7 @@ Rules:
         )
 
         response = client.chat.completions.create(
-            model="llama-3.3-70b-versatile",
+            model="openai/gpt-oss-20b",
             messages=[
                 {
                     "role": "system",
